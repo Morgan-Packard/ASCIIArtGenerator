5d4ace92ecfe33838c0af78c36a2ba315e3feaae
Morgan Packard <packard25m@ncssm.edu>
2024-08-06 21:36:58 -0400

    Got C++ working and added note with a library adding tutorial. This project will be a good experience for intro to APIs. Having fun XD!

---
+++ main.cpp
@@ -0,0 +1,10 @@
+/*
+I need to include a png/jpg reader, so I'll use this to figure out how to pull from a library.
+https://www.reddit.com/r/cpp_questions/comments/v6pgei/what_is_the_best_way_to_include_github_libraries/
+
+*/
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+}
